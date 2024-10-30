fe631748c5b0c90f231a5cd6736c0f20e3ba4b32
Igor Souza <103651578+Isouz@users.noreply.github.com>
2024-10-30 19:48:09 -0300

    adicionei um hover nas imagens

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('botao_conheca_planos').addEventListener('click', function() {
+    const planos = document.getElementById('planos');
+    planos.scrollIntoView({ behavior: 'smooth' });
+});
+
